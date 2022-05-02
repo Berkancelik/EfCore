cc226231ab0e8107cbf9d7f1b1bd3a9e2f3bea0b
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-02 10:26:07 +0300

    Scaffold Operation bt Db Invalid error

---
+++ EFCore.DatabaseFirstByScaffold/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore.DatabaseFirstByScaffold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- EfCoreDatabaseFirst/Program.cs
+++ EfCoreDatabaseFirst/Program.cs
@@ -9,6 +9,6 @@ using (var _context = new AppDbContext())
     var products = await _context.Products.ToListAsync();
     products.ForEach(p =>
     {
-        Console.WriteLine($"{p.Id}:{p.Name}");
+        Console.WriteLine($"{p.Id}:{p.Name}:{p.Price}:{p.Stock}");
     });
 }
