752c6851bf22535e5de74f270e4f44ca7dc20b9a
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-03 12:07:41 +0300

    DbContext (ChangeTracker/ ContextId / Database)

--- EfCore.CodeFirst/DAL/Product.cs
+++ EfCore.CodeFirst/DAL/Product.cs
@@ -10,8 +10,9 @@ namespace EfCore.CodeFirst.DAL
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public int Stock { get; set; }
         public int Barcode { get; set; }
+        public DateTime? CreatedDate { get; set; }
     }
 }
---
+++ EfCore.CodeFirst/Migrations/20220503083450_AddCreatedData_ForProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCore.CodeFirst.Migrations
+{
+    public partial class AddCreatedData_ForProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Products");
+        }
+    }
+}
