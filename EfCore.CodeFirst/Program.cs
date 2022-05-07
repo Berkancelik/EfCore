f390b722f2cae8180f0cd4d736079d283d54d654
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-07 12:12:15 +0300

    Explicit Loading

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -8,30 +8,12 @@ using System.Linq;
 Initializer.Build();
 using (var _context = new AppDbContext())
 {
-    var category = new Category() { Name = "Kalemler" };
-    category.Products.Add(new Product()
+     var product = _context.Products.First();
+    if (true)
     {
-        Name = "Kalem 1",
-        Price = 100,
-        Stock = 100,
-        Barcode = 123,
-        ProductFeature = new() { Color = "Red", Height = 100, Width = 200 }
-    });
-
-         category.Products.Add(new Product()
-         {
-             Name = "Kalem 2",
-             Price = 100,
-             Stock = 100,
-             Barcode = 123,
-             ProductFeature = new() { Color = "Red", Height = 100, Width = 200 }
-         });
-
-    var categoryWithProducts = _context.Categories.Include(x => x.Products).ThenInclude(x=>x.ProductFeature).First();
-
-    await _context.AddAsync(category);    
-     _context.SaveChanges(); 
-
+        _context.Entry(product).Reference(x => x.ProductFeature).Load();
+   
+    }
 }
 
 
