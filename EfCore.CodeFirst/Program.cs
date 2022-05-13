deeefffc7bfaef819f380f37fb0099d37aaafafa
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-13 12:17:31 +0300

    Full Outer Join

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -6,38 +6,38 @@ using System.Collections.Generic;
 using System.Linq;
 
 Initializer.Build();
-using (var _context = new AppDbContext()) 
+using (var _context = new AppDbContext())
 {
+    var left = await (from p in _context.Products
+                      join pf in _context.ProductFeature on p.Id equals pf.Id into pfList
+                      from pf in pfList.DefaultIfEmpty()
+                      select new
+                      {
+                          Id = p.Id,
+                          Name = p.Name,
+                          Color = pf.Color
 
-    var result = _context.Categories
-        .Join(_context.Products, c => c.Id, p => p.CategoryId, (c, p) => new
-        {
-            c,
-            p
-        }).Join(_context.ProductFeature,x=>x.p.Id,y=>y.Id(c,pf)=> new
-        {
-            CategoryName = c.c.Name,
-            ProduvtName = c.p.Name,
-            ProductFeature = pf.Color
-        });
+                      }).ToListAsync();
 
-    //3 lü join
-    var result2 = (from c in _context.Categories
-                   join p in _context.Products on c.Id equals p.CategoryId
-                   join pf in _context.ProductFeature on p.Id equals pf.Id
-                   select new { c, p, pf }).ToList();  
+    // Query syntax
+    var right = await (from pf in _context.ProductFeature
+                       join p in _context.Products on pf.Id equals p.Id into pList
+                       from p in pList.DefaultIfEmpty()
+                       select new
+                       {
+                           Id = p.Id,
+                           Name = p.Name,
+                           Color = pf.Color
 
-    // sadece join metodu vardır burada. Bu kategoriye bağlı
-    //var result = _context.Categories.Join(_context.Products, x => x.Id, y => y.CategoryId, (c, p) => p).ToList();
+                       }).ToListAsync();
 
 
-    //var resul2 = (from c in _context.Categories 
-    //             join p in _context.Products on c.Id equals p.Id
-    //             select new {
-    //             CategoryName = c.Name,
-    //             ProductName = c.Name,
-    //             ProductPrice = p.Price
-    //             }).ToList();
+    var outerJoin = left.Union(right);
+
+
+
+
+    Console.WriteLine("");
 
 
 
@@ -47,6 +47,6 @@ using (var _context = new AppDbContext())
     //category.Products.Add(new() { Name = "kalem 3", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
     //category.Products.Add(new() { Name = "kalem 4", Price = 100, Stock = 200, Barcode = 123 });
     //_context.Categories.Add(category);
-    _context.SaveChanges();
+    //_context.SaveChanges();
     Console.WriteLine("işlem bitti");
 }
