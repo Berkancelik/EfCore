dd9c37534a321f0ee5ee0950acd12dfeee4d21db
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-16 17:46:56 +0300

    Anonymous Types - 1

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -8,9 +8,23 @@ using System.Linq;
 Initializer.Build();
 using (var _context = new AppDbContext())
 {
-    var product = _context.Products.ToList();
+    var product = await _context.Products.Include(x=>x.Category).Include(x => x.ProductFeature).Select(x=> new
+    {
+        CategoryName = x.Category.Name,
+        ProductName = x.Name,
+        ProductPrice = x.Price,
+        width=(int?)x.ProductFeature.Width
+    }).Where(x=>x.width >10).ToListAsync();
 
-    Console.WriteLine("");
+    var categories = _context.Categories.Include(x => x.Products).ThenInclude(x => x.ProductFeature).Select(x => new
+    {
+        CategoryName = x.Name,
+        Products = String.Join(",", x.Products.Select(z => z.Name)),
+        TotalPrice = x.Products.Sum(x => x.Price)
+    }).Where(y=>y.TotalPrice>100).OrderBy(x=>x.TotalPrice).ToList();
+
+
+  Console.WriteLine("");
     #region DataInsert
     //var category = new Category() { Name = "Defterler" };
     //category.Products.Add(new() { Name = "Defter 1", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
